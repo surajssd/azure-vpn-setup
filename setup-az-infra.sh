1cf7763df617f5c40ecea17bef327b24bdf9c989
Suraj Deshmukh <surajd.service@gmail.com>
2023-07-30 17:23:29 -0700

    Add arguments to the script
    Signed-off-by: Suraj Deshmukh <surajd.service@gmail.com>

--- setup-az-infra.sh
+++ setup-az-infra.sh
@@ -11,25 +11,44 @@ export user_name="azuser"
 export ssh_key="~/.ssh/id_rsa.pub"
 export dns_name="vpn-server-${index}"
 
-az group create --name "${resource_group}" --location "${location}"
+function setup() {
+    az group create --name "${resource_group}" --location "${location}"
 
-az vm create \
-    --resource-group "${resource_group}" \
-    --size Standard_B2s \
-    --name "${vm_name}" \
-    --image canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:latest \
-    --location "${location}" \
-    --public-ip-address-dns-name "${dns_name}" \
-    --admin-username "${user_name}" \
-    --ssh-key-values "${ssh_key}"
+    az vm create \
+        --resource-group "${resource_group}" \
+        --size Standard_B2s \
+        --name "${vm_name}" \
+        --image canonical:0001-com-ubuntu-server-jammy:22_04-lts-gen2:latest \
+        --location "${location}" \
+        --public-ip-address-dns-name "${dns_name}" \
+        --admin-username "${user_name}" \
+        --ssh-key-values "${ssh_key}"
 
-# Open ssh port 22 and 51820
-az vm open-port \
-    --port 22,51820 \
-    --resource-group "${resource_group}" \
-    --name "${vm_name}"
+    # Open ssh port 22 and 51820
+    az vm open-port \
+        --port 22,51820 \
+        --resource-group "${resource_group}" \
+        --name "${vm_name}"
 
-URL="${dns_name}.${location}.cloudapp.azure.com"
-echo $URL
+    URL="${dns_name}.${location}.cloudapp.azure.com"
+    echo $URL
 
-echo "ssh ${user_name}@${URL}"
+    echo "ssh ${user_name}@${URL}"
+}
+
+function ssh() {
+    URL="${dns_name}.${location}.cloudapp.azure.com"
+    set -x
+    exec ssh "${user_name}@${URL}"
+}
+
+# Call appropriate function as per the argument
+if [ "$1" == "setup" ]; then
+    setup
+elif [ "$1" == "ssh" ]; then
+    ssh
+else
+    echo "Err: Invalid argument"
+    echo "./setup-az-infra.sh setup"
+    echo "./setup-az-infra.sh ssh"
+fi
